eb3b81d4b0a6e6251259db961c45233299eba789
Ryan Lee <36366673+ryanlee30@users.noreply.github.com>
2020-09-16 23:59:32 -0700

    Update console.js

--- console.js
+++ console.js
@@ -16,40 +16,32 @@ function checkInput(e) {
             break;
             case 'help':
             window.alert('Valid Commands:\nresume - View my Resume\ngithub - Open my GitHub page\nlinkedin - Open my LinkedIn page');
-            input.value = "";
             break;
             case 'Help':
             window.alert('Valid Commands:\nresume - View my Resume\ngithub - Open my GitHub page\nlinkedin - Open my LinkedIn page');
-            input.value = "";
             break;
             case 'github':
             window.open('https://github.com/ryanlee30/');
-            input.value = "";
             break;
             case 'Github':
             window.open('https://github.com/ryanlee30/');
-            input.value = "";
             break;
             case 'resume':
             window.open('/files/resume.pdf', 'pdf');
-            input.value = "";
             break;
             case 'Resume':
             window.open('/files/resume.pdf', 'pdf');
-            input.value = "";
             break;
             case 'linkedin':
             window.open('https://www.linkedin.com/in/ryanlee30/');
-            input.value = "";
             break;
             case 'Linkedin':
             window.open('https://www.linkedin.com/in/ryanlee30/');
-            input.value = "";
             break;
             default:
             window.alert('Wrong command!\nType "help" for valid commands');
-            input.value = "";
         }
+        input.value = "";
     }
 }
 
